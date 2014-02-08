1d79ebcfec8979db96c1aa3c266d9c0553cbfbf5
Joey Caero <joeycaero@gmail.com>
2014-02-08 09:03:43 -0600

    basic tracking added

--- CFMacro/Window.Designer.cs
+++ CFMacro/Window.Designer.cs
@@ -28,20 +28,67 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
+            this.GUITick = new System.Windows.Forms.Timer(this.components);
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.stockPrice = new System.Windows.Forms.Label();
+            this.lastTradePriceText = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
             this.SuspendLayout();
             // 
-            // Form1
+            // GUITick
+            // 
+            this.GUITick.Enabled = true;
+            this.GUITick.Tick += new System.EventHandler(this.GUITick_Tick);
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.Location = new System.Drawing.Point(11, 10);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(507, 479);
+            this.pictureBox.TabIndex = 1;
+            this.pictureBox.TabStop = false;
+            // 
+            // stockPrice
+            // 
+            this.stockPrice.AutoSize = true;
+            this.stockPrice.Location = new System.Drawing.Point(134, 506);
+            this.stockPrice.Name = "stockPrice";
+            this.stockPrice.Size = new System.Drawing.Size(119, 13);
+            this.stockPrice.TabIndex = 2;
+            this.stockPrice.Text = "YHOO LastTradePrice: ";
+            // 
+            // lastTradePriceText
+            // 
+            this.lastTradePriceText.AutoSize = true;
+            this.lastTradePriceText.Location = new System.Drawing.Point(259, 506);
+            this.lastTradePriceText.Name = "lastTradePriceText";
+            this.lastTradePriceText.Size = new System.Drawing.Size(10, 13);
+            this.lastTradePriceText.TabIndex = 3;
+            this.lastTradePriceText.Text = "-";
+            // 
+            // Window
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(964, 558);
-            this.Name = "Form1";
+            this.ClientSize = new System.Drawing.Size(529, 535);
+            this.Controls.Add(this.lastTradePriceText);
+            this.Controls.Add(this.stockPrice);
+            this.Controls.Add(this.pictureBox);
+            this.Name = "Window";
             this.Text = "*";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Timer GUITick;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label stockPrice;
+        private System.Windows.Forms.Label lastTradePriceText;
     }
 }
 
--- CFMacro/Window.cs
+++ CFMacro/Window.cs
@@ -7,14 +7,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using System.Web;
 
 namespace CFMacro
 {
+    public delegate void UpdateChartCallback( System.IO.MemoryStream memoryStream);
+    public delegate void GetStockInfo(string stockInfo);
     public partial class Window : Form
     {
+        private DataManager dataManager;
+
         public Window()
         {
             InitializeComponent();
+            dataManager = new DataManager(updateChart, updateStockInfo);
+        }
+
+        public void updateChart(System.IO.MemoryStream memoryStream) {
+            pictureBox.Image = System.Drawing.Image.FromStream(memoryStream);
+        }
+
+        public void updateStockInfo(string stockInfo) {
+            lastTradePriceText.Text = stockInfo;
+        }
+        private void GUITick_Tick(object sender, EventArgs e) {
+
         }
     }
 }
